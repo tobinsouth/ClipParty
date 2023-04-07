f009ebe76d770be45a4ebc048bbd0b2084de69da
unknown <jason.hou@berkeley.edu>
2023-04-07 15:56:32 -0400

    graffiti is the best font

--- printer.py
+++ printer.py
@@ -2,6 +2,25 @@ import serial
 import adafruit_thermal_printer
 from art import *
 
+clip_small= """
+        ,@*\"\"\"*g
+        ,,gF      ]&
+    y@*[J$$\"     ,g,
+    y`,gNgg \",  ,w&Z$Bg
+    \ ]@@@@  @` g@@N '$*
+    ^~wwe\" ]  N@@@` /
+        ][ ,   `^mr^\"-
+        ][ $     $F /F
+        ][ $     ]L $
+        ]$ $     ]F]F
+        $ ]k    $F]F
+        $  \"k, ,@ ]F
+        ]k   `\"'  ]F
+        ]k       $
+            '&w, ,$\"
+            '\"\"\"
+"""
+
 def printerHardcore(text, port):
     ThermalPrinter = adafruit_thermal_printer.get_printer_class(2.69)
     uart = serial.Serial(port, baudrate=19200, timeout=3000)
